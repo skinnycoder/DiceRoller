90419638d89abf826ba527c1a4320eabf053ac37
skinnycoder <ronnlefeuvre@gmail.com>
2024-01-25 22:04:33 -0800

    Revert "Unreal Project Added"
    This reverts commit c1c07fa1760d2b7ea0fd5f680e80eac513dfce15.

--- Source/DiceRoller/DiceRoller.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "DiceRoller.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DiceRoller, "DiceRoller" );
-
-DEFINE_LOG_CATEGORY(LogDiceRoller)
- 
