eb01bda3b4cb8a0d6d3e29dadfd9d43209b020e6
skinnycoder <ronnlefeuvre@gmail.com>
2024-01-28 16:33:58 -0800

    Checking in current version

---
+++ Source/DiceRoller/DiceRoller.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DiceRoller.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DiceRoller, "DiceRoller" );
+
+DEFINE_LOG_CATEGORY(LogDiceRoller)
+ 
